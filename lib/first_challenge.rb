e5f4d0cbc008e762f3d5e6c93795a2b8ae52e839
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-14 03:55:39 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,8 +17,7 @@ def first_challenge
 contacts.each do |person,data|
   data.each do |attribute, value|
     if attribute == :favorite_icecream_flavors
-      value.each do |flavor|
-        value.delete_if flavor == "strawberry"
+        value.delete_if {|flavor| flavor == "strawberry"
       end
     end
   end
