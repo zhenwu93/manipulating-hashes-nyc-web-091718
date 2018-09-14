a75c24d50401f4003f4328c3bb72c0ed2925ecfd
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-14 03:50:14 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,7 +2,7 @@ def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -23,4 +23,3 @@ contacts.each do |person,data|
   #remember to return your newly altered contacts hash!
   contacts
 end
-
